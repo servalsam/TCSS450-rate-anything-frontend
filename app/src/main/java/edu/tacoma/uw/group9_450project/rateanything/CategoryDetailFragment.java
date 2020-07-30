09a1234d763745861ec5abd881c5ab516298e356
richw253 <44218255+richw253@users.noreply.github.com>
2020-07-30 12:27:33 -0700

    Added Code to connect our database to the app. Right now the landing screen shows the categories and can show category details.

---
+++ app/src/main/java/edu/tacoma/uw/group9_450project/rateanything/CategoryDetailFragment.java
@@ -0,0 +1,71 @@
+package edu.tacoma.uw.group9_450project.rateanything;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import edu.tacoma.uw.group9_450project.rateanything.model.Category;
+import edu.tacoma.uw.group9_450project.rateanything.model.CategoryContent;
+
+/**
+ * A fragment representing a single Category detail screen.
+ * This fragment is either contained in a {@link CategoryListActivity}
+ * in two-pane mode (on tablets) or a {@link CategoryDetailActivity}
+ * on handsets.
+ */
+public class CategoryDetailFragment extends Fragment {
+
+    /** The fragment argument representing the item ID that this fragment represents. */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /** The category content this fragment is presenting. */
+    private Category mCategory;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public CategoryDetailFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the course content specified by the fragment arguments
+            mCategory = (Category) getArguments().getSerializable(ARG_ITEM_ID);
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout =
+                    (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mCategory.getCategoryID());
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.category_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mCategory != null) {
+            ((TextView) rootView.findViewById(R.id.item_detail_id))
+                    .setText(mCategory.getCategoryID());
+            ((TextView) rootView.findViewById(R.id.item_detail_name))
+                    .setText(mCategory.getMyCategoryName());
+            ((TextView) rootView.findViewById(R.id.item_detail_short_desc))
+                    .setText(mCategory.getMyCategoryShortDesc());
+            ((TextView) rootView.findViewById(R.id.item_detail_long_desc))
+                    .setText(mCategory.getMyCategoryLongDesc());
+        }
+        return rootView;
+    }
+}
---
+++ app/src/main/java/edu/tacoma/uw/group9_450project/rateanything/CategoryListActivity.java
@@ -0,0 +1,237 @@
+package edu.tacoma.uw.group9_450project.rateanything;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import edu.tacoma.uw.group9_450project.rateanything.model.Category;
+import edu.tacoma.uw.group9_450project.rateanything.model.CategoryContent;
+
+/**
+ * An activity representing a list of Categories. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link CategoryDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ * @author Code supplied by Android Studio and UWT 450 Instructor. Updated
+ * by Rich
+ */
+public class CategoryListActivity extends AppCompatActivity {
+
+    /** ArrayList for categories and RecyclerView must be member variables accessible outside
+     * of onCreate.
+     */
+    private List<Category> mCategoryList;
+    private RecyclerView mRecyclerView;
+
+    /**
+     * Private class to setup asynchronous loading of the data.
+     * Code supplied by UWT 450 Instructor. Modified by Rich W.
+     */
+    private class CategoryTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to download the list of courses, Reason: "
+                            + e.getMessage();
+                }
+                finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s.startsWith("Unable to")) {
+                Toast.makeText(getApplicationContext(), "Unable to download" + s,
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+
+                if (jsonObject.getBoolean("success")) {
+                    mCategoryList = Category.parseCategoryJson(
+                            jsonObject.getString("categories")); // Name of table
+                    if(!mCategoryList.isEmpty()) {
+                        setupRecyclerView((RecyclerView) mRecyclerView);
+                    }
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "JSON Error: " + e.getMessage(),
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        if (findViewById(R.id.item_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+
+        /** This code is used to instantiate mRecyclerView */
+        mRecyclerView= findViewById(R.id.item_list);
+        assert mRecyclerView != null;
+        setupRecyclerView((RecyclerView) mRecyclerView);
+    }
+
+    /**
+     * The onResume method is used to launch the AsyncTask.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mCategoryList == null) {
+            new CategoryTask().execute(getString(R.string.get_categories));
+        }
+    }
+
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        if (mCategoryList != null) {
+            mRecyclerView.setAdapter(new SimpleItemRecyclerViewAdapter
+                    (this, mCategoryList, mTwoPane));
+        }
+    }
+
+    public static class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final CategoryListActivity mParentActivity;
+        private final List<Category> mValues;
+        private final boolean mTwoPane;
+        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Category item = (Category) view.getTag();
+                if (mTwoPane) {
+                    Bundle arguments = new Bundle();
+                    arguments.putSerializable(CategoryDetailFragment.ARG_ITEM_ID, item);
+                    CategoryDetailFragment fragment = new CategoryDetailFragment();
+                    fragment.setArguments(arguments);
+                    mParentActivity.getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.item_detail_container, fragment)
+                            .commit();
+                } else {
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, CategoryDetailActivity.class);
+                    intent.putExtra(CategoryDetailFragment.ARG_ITEM_ID, item);
+
+                    context.startActivity(intent);
+                }
+            }
+        };
+
+        SimpleItemRecyclerViewAdapter(CategoryListActivity parent,
+                                      List<Category> items,
+                                      boolean twoPane) {
+            mValues = items;
+            mParentActivity = parent;
+            mTwoPane = twoPane;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.category_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mIdView.setText(mValues.get(position).getCategoryID());
+            holder.mContentView.setText(mValues.get(position).getMyCategoryName());
+
+            holder.itemView.setTag(mValues.get(position));
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            final TextView mIdView;
+            final TextView mContentView;
+
+            ViewHolder(View view) {
+                super(view);
+                mIdView = (TextView) view.findViewById(R.id.id_text);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+        }
+    }
+}
