08b7d363236f53c0fcdfd5563605b189d7d505fd
swainright <samuelwainright@gmail.com>
2020-07-29 17:19:24 -0700

    Committing to fix own gradle.

--- app/src/test/java/edu/tacoma/uw/group9_450project/rateanything/ExampleUnitTest.java
+++ app/src/test/java/edu/tacoma/uw/group9_450project/rateanything/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package edu.tacoma.uw.group9_450project.rateanything;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
